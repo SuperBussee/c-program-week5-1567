85194479a709b3b93611257fa1f81d5f027d8b40
SuperBussee <tenz134120@gmail.com>
2024-06-21 15:08:09 +0700

    first commit

---
+++ app501.c
@@ -0,0 +1,19 @@
+//header
+#include<stdio.h>
+int x = 0b1011;
+int y = 0x10;
+int z = 10;
+//body
+int main(){
+    printf("x in Decimal :%d", x);
+    printf("x in Octal :%o", x);
+    printf("x in Hexadecimal :%x\n\n", x);
+    
+    printf("x in Decimal :%d", y);
+    printf("x in Octal :%o", y);
+    printf("x in Hexadecimal :%x\n\n", y);
+
+    printf("x in Decimal :%d", z);
+    printf("x in Octal :%o", z);
+    printf("x in Hexadecimal :%x\n\n", z);
+}
---
+++ app502.c
@@ -0,0 +1,21 @@
+//header
+#include<stdbool.h>
+int x = 65;// Decimal = 10
+int y = 33;// Decimal =10
+bool isMarry = true;
+bool isMale = false;
+//body
+void main(){
+    printf("x in Decimal :%d", x);
+    printf("x in Octal :%o", x);
+    printf("x in Hexadecimal :%x\n", x);
+    printf("x in character :%c\n\n", x);
+    printf("isMarry in bool :%d\n\n", isMarry);
+    
+    printf("x in Decimal :%d", y);
+    printf("x in Octal :%o", y);
+    printf("x in Hexadecimal :%x\n\n", y);
+    printf("x in character :%c\n\n", y);
+    printf("isMale in bool :%d\n\n", isMale);
+    return 0;
+}
